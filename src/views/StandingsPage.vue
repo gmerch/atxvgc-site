4b0f71a35d289174d08dc30804971bc1598eb63f
gmerch <gemma.merch@gmail.com>
2020-10-14 20:59:15 -0500

    Added Modals on Standings Page

--- src/views/StandingsPage.vue
+++ src/views/StandingsPage.vue
@@ -1,7 +1,27 @@
 //src/views/StandindgsPage.vue
 <template>
   <div>
-    <b-table :fields="fields" striped hover :items="items"></b-table>
+    <b-table :fields="fields" striped hover :items="players" responsive="sm">
+      <template v-slot:cell(name)="player">
+        <b class="text-info" v-b-modal="'modal-1'" @click="loadPlayerInfo(player.item)">{{ player.item.name }}</b>
+      </template>
+      <template v-slot:cell(wins)="player">
+        <b class="text-info">{{ player.item.wins }}</b>
+      </template>
+      <template v-slot:cell(games_played)="player">
+        <b class="text-info">{{ player.item.games_played }}</b>
+      </template>
+      <template v-slot:cell(win_pct)="data">
+        <b class="text-info">{{ data.item.win_pct }}</b>
+        </template>
+    </b-table>
+    <b-modal id="modal-1" @hidden="clearPlayerInfo">
+      <template>
+        <p>Name: {{ player.name }} </p>
+        <p>Twitter: <a :href="'https://twitter.com/'+player.twitter">{{ player.twitter }}</a></p>
+        <p>Twitch: <a :href="'https://twitch.com/'+player.twitch">{{ player.twitch }}</a></p>
+      </template>
+    </b-modal>
   </div>
 </template>
 <style scoped>
@@ -11,27 +31,62 @@
 </style>
 <script>
 import {atxAPI} from '../api/index.js'
+
 export default {
     data() {
       return {
-        items: []
+        fields: [],
+        players: [],
+        playerInfo: [],
+        player: {}
       }
     },
     mounted(){
       console.log('testing 123')
-      this.fields = ['name','wins','games_played','win_%']
+      this.players = [];
+      console.log('one last attempt', this.playerData)
+      this.fields = [
+        {
+          key:'name',
+          sortable:false
+        },
+        {
+          key:'wins',
+          sortable:true
+        },
+        {
+          key:'games_played',
+          sortable:true
+        },
+        {
+          key:'win_pct',
+          label: 'Win Percentage',
+          sortable:true
+        }
+      ]
     },
     created () {
       atxAPI
         .get('table')
         .then(response => {
-          this.items = response.data
-          console.log('what')
+          this.players = response.data
           }
         )
         .catch(e => {
           console.log(e)
         })
+    },
+    methods: {
+      loadPlayerInfo(item){
+        console.log('Loading!')
+        console.log(this)
+        this.player = item
+        console.log('player', this.player)
+        this.$bvModal.show('modal-1')
+      },
+      clearPlayerInfo(){
+        this.player = {};
+      }
     }
   }
 </script>
