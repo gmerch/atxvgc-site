cec46af2c7a9aa442ac12db41a197ff0f12b5bae
gmerch <gemma.merch@gmail.com>
2020-10-22 10:24:35 -0500

    Updating styles

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h2>Hello, testing</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <b-row width=100%>
   <b-navbar toggleable="lg" type="light" variant="faded" stciky=true class="fluid container-fluid">
-    <b-col cols=2>
+    <b-col cols=3.5>
     <b-navbar-brand href="#"><img src="../assets/logo.png" alt="Logo" width="89.5" height="120"></b-navbar-brand>
     </b-col>
-    <b-col fluid="md" cols=8>
+    <b-col fluid="md">
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
@@ -26,7 +26,7 @@
       <!-- Right aligned nav items -->
     </b-collapse>
     </b-col>
-    <b-col fluid="sm" cols=2>
+    <b-col fluid="sm">
     <a href="https://twitch.tv/atx_vgc" target="_blank" rel="noopener noreferrer"><img class="social-icon" src="../assets/logos/twitch-circle-logo.png"></a>
     <a href="https://twitter.com/atxvgc" target="_blank" rel="noopener noreferrer"><img class="social-icon" src="../assets/logos/twitter-circle-logo.png"></a>
     <a href="https://discord.gg/axJgqGg" target="_blank" rel="noopener noreferrer"><img class="social-icon" src="../assets/logos/discord-circle-logo.png"></a>
