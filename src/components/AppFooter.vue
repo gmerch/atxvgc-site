b7a32e48c8f48a26c71d3ff174502773c84fdd2d
gmerch <gemma.merch@gmail.com>
2020-10-28 16:22:54 -0500

    Updated footer and forced text to be black unless in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,29 @@
 <template>
-  <div>
-    <p>Content &copy; 2020 ATX VGC, LLC All Rights Reserved</p>
-  </div>
+  <b-container class="footer ml-0 mr-0">
+    <b-row class="mb-3">
+    <b-col cols=4><img class='footer-img' src='../assets/logo-vertical-new.png'></b-col>
+    </b-row>
+    <b-row class="mb-0">
+      <b-col cols=8><p class='footer-txt'>ATX VGC is Home to Austin Texas' VGC Community! Feel free to join us on all of the socials and get involved!</p></b-col>
+    </b-row>
+    <b-row class="mb-0">
+      <b-col cols=8>Content&copy; 2020 ATX VGC, LLC All Rights Reserved</b-col>
+    </b-row>
+  </b-container>
 </template>
+<style scoped>
+  .footer {
+    background: black;
+    color:white;
+    max-width: none;
+  }
+  .footer-img {
+    max-width: 250px;
+  }
+  .footer-txt {
+    color:white;
+  }
+</style>
 <script>
 export default {
     data: () => {
@@ -10,5 +31,6 @@ export default {
         results: [],
       };
     },
+
 }
 </script>
